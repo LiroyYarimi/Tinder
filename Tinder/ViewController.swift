0ba3103d3a4052afd6a3ac93a3e22393ca16edbd
liroy y <liroy.yarimi@gmail.com>
2019-02-12 20:50:14 +0200

    Create stack view on the main page

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -12,7 +12,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let topViewHeight : CGFloat = 150
+        let yellowViewHeight : CGFloat = 120
+        
+        let subviews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { (color) -> UIView in
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+        }
+        let topStackView = UIStackView(arrangedSubviews: subviews) //cmd + control + E = rename varible
+        topStackView.distribution = .fillEqually
+        topStackView.heightAnchor.constraint(equalToConstant: topViewHeight).isActive = true
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: yellowViewHeight).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackView,blueView,yellowView])
+        stackView.axis = .vertical
+        stackView.frame = .init(x: 0, y: 0, width: 300, height: 200)
+        
+        view.addSubview(stackView)
+        stackView.fillSuperview()
     }
 
 
